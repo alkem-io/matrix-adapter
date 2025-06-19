24bc016a597e7a1cb9e73606204692763bb44642
Neil Smyth <neil@thesmyths.eu>
2025-06-19 09:29:37 +0200

    tidy up scripts

--- service/test/config/vitest.config.unit.ts
+++ service/test/config/vitest.config.unit.ts
@@ -1,6 +1,5 @@
 import swc from "unplugin-swc";
 import { defineConfig } from "vitest/config";
-
 import { createVitestTestConfig } from "./create-vitest-test-config";
 
 export default defineConfig({
