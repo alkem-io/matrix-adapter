09f129361da2bec2394edbfd82ad03d7b2311150
Neil Smyth <neil@thesmyths.eu>
2025-07-13 08:48:28 +0200

    fix module import

--- service/src/core/bootstrap/bootstrap.module.ts
+++ service/src/core/bootstrap/bootstrap.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BootstrapService } from './bootstrap.service';
 import { MatrixAdminUserElevatedModule } from '@src/domain/matrix-admin/user-elevated/matrix.admin.user.elevated.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [MatrixAdminUserElevatedModule],
+  imports: [MatrixAdminUserElevatedModule, HttpModule],
   providers: [BootstrapService],
   exports: [BootstrapService],
 })
